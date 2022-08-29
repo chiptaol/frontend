420a7a568e3ab0f7ba22b82b6c79c0ed63cbc9a8
dontmeway <bbakhadyrov@gmail.com>
2022-08-29 12:01:32 +0500

    return images

--- src/entities/premiere/ui/slider.tsx
+++ src/entities/premiere/ui/slider.tsx
@@ -67,14 +67,14 @@ export const PremieresSlider = () => {
           <Link href={routesMap.premiere(premiere.id)}>
             <a>
               <div className="relative w-full h-52 sm:h-72 flex items-end overflow-hidden rounded-lg bg-red-50">
-                {/* <Image
+                <Image
                   priority
                   layout="fill"
                   quality={100}
                   className="absolute inset-0 object-cover"
                   src={imageSrc(premiere.backdrop_path)}
                   alt="slider_image"
-                /> */}
+                />
                 <div className="flex flex-col space-y-3 relative px-3 py-7 bg-sliderContent w-full">
                   <h4 className="font-semibold text-lg leading-5 truncate w-3/4">
                     {premiere.title}
@@ -91,3 +91,5 @@ export const PremieresSlider = () => {
     </div>
   )
 }
+
+function autoPlay() {}
