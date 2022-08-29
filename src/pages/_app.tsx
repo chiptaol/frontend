a8a8ff0493e6ee185be6c8e70f971830aeebb117
dontmeway <bbakhadyrov@gmail.com>
2022-08-29 15:55:45 +0500

    added loader for pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { fork, serialize } from 'effector'
 import { Provider } from 'effector-react/scope'
+import Router from 'next/router'
 import dayjs from 'dayjs'
 import ru from 'dayjs/locale/ru'
 import customParseFormat from 'dayjs/plugin/customParseFormat'
-import type { ReactElement } from 'react'
+import NProgress from 'nprogress'
 import type { Scope } from 'effector'
+import type { ReactElement } from 'react'
 
+import 'nprogress/nprogress.css'
 import 'keen-slider/keen-slider.min.css'
 
 import { Layout } from '~widgets/layout'
@@ -16,6 +19,10 @@ import '~shared/styles/font.css'
 
 dayjs.locale(ru)
 dayjs.extend(customParseFormat)
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 let clientScope: Scope
 
