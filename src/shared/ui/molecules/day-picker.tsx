1ee81b114217feff23b276157863003953f70039
dontmeway <bbakhadyrov@gmail.com>
2022-08-31 15:52:40 +0500

    fix: padding right of horizontally scrolable element

--- src/shared/ui/molecules/day-picker.tsx
+++ src/shared/ui/molecules/day-picker.tsx
@@ -29,20 +29,18 @@ type DayPickerItemProps = {
 
 const DayPickerItem = (props: DayPickerItemProps) => {
   return (
-    <div>
-      <button
-        onClick={props.onClick}
-        type="button"
-        className={cn(
-          'flex flex-col justify-center items-center px-2 py-2.5 space-y-1.5 rounded-xl cursor-pointer shrink-0 transition-colors',
-          {
-            'bg-white bg-opacity-10': props.isActive,
-          }
-        )}
-      >
-        <span className={cn('text-sm leading-4 font-semibold')}>{props.day.dayOrDate}</span>
-        <span className={cn('text-sm leading-4 text-gray-300')}>{props.day.weekDay}</span>
-      </button>
-    </div>
+    <button
+      onClick={props.onClick}
+      type="button"
+      className={cn(
+        'flex flex-col justify-center items-center px-2 py-2.5 space-y-1.5 rounded-xl cursor-pointer shrink-0 transition-colors',
+        {
+          'bg-white bg-opacity-10': props.isActive,
+        }
+      )}
+    >
+      <span className={cn('text-sm leading-4 font-semibold')}>{props.day.dayOrDate}</span>
+      <span className={cn('text-sm leading-4 text-gray-300')}>{props.day.weekDay}</span>
+    </button>
   )
 }
