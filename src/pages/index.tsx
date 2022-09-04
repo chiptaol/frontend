e1e9c7f7f9c17d443237c988ab3b615fc94d266b
dontmeway <bbakhadyrov@gmail.com>
2022-09-04 21:42:57 +0500

    og image added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,6 +44,8 @@ const Helmet = () => {
       <meta property="og:title" content="Chiptaol - Онлайн-покупка билетов в кинотеатры Ташкента" />
       <meta property="og:locale" content="ru_RU" />
       <meta property="og:site_name" content="Chiptaol" />
+      <meta property="og:url" content="https://chiptaol.uz" />
+      <meta prefix="og: https://chiptaol.uz/ns#" />
       <meta property="og:image" content="https://chiptaol.uz/og-image.png" />
       <meta property="og:image:width" content="1280" />
       <meta property="og:image:height" content="730" />
