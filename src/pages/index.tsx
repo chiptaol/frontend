3714092531ab5102079f993112e36d760039b6a0
dontmeway <bbakhadyrov@gmail.com>
2022-09-04 21:15:50 +0500

    og image added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,6 +44,7 @@ const Helmet = () => {
       <meta property="og:title" content="Chiptaol - Онлайн-покупка билетов в кинотеатры Ташкента" />
       <meta property="og:locale" content="ru_RU" />
       <meta property="og:site_name" content="Chiptaol" />
+      <meta property="og:image" content="/og-image.png" />
       <meta
         property="og:description"
         content="Надоело стоять в кассах или приходить, когда уже все билеты распроданы? Для этого не обязательно ехать в кинотеатр, Вы можете купить билеты онлайн у нас."
